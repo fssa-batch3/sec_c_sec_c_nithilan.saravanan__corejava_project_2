df1b46ebe8186bafb946b622eab1d9afebff6dfb
nithilans <110275969+nithilans@users.noreply.github.com>
2023-08-08 15:09:59 +0530

    Add files via upload

--- src/main/java/com/fssa/freshfood/dao/ConnectionUtil.java
+++ src/main/java/com/fssa/freshfood/dao/ConnectionUtil.java
@@ -2,48 +2,40 @@ package com.fssa.freshfood.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class ConnectionUtil {
 
-	// Method to establish a database connection
-	public static Connection getConnection() {
+	 public static Connection getConnection() {
+	        Connection con = null;
 
-		Connection con = null;
-		String url = "jdbc:mysql://localhost:3306/freshfood"; // Database URL
-		String userName = "root"; // Database username
-		String passWord = "123456"; // Database password
+	        String url, userName, passWord;
 
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver"); // Load the MySQL JDBC driver class
-			con = DriverManager.getConnection(url, userName, passWord); // Establish a connection to the database
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("Unable to connect to the database"); // Throw a runtime exception if connection
-																				// fails
-		}
+	        if (System.getenv("CI") != null) {
+	            url = System.getenv("DATABASE_HOST");
+	            userName = System.getenv("DATABASE_USERNAME");
+	            passWord = System.getenv("DATABASE_PASSWORD");
+	        } else {
+	            Dotenv env = Dotenv.load();
+	            url = env.get("DATABASE_HOST");
+	            userName = env.get("DATABASE_USERNAME");
+	            passWord = env.get("DATABASE_PASSWORD");
+	        }
 
-		return con;
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	            con = DriverManager.getConnection(url, userName, passWord);
+	            System.out.println("Connected");
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            throw new RuntimeException("Unable to connect to the database");
+	        }
+	        return con;
+	    }
+	 
+	 public static void main(String[] args) {
+		getConnection();
 	}
 
-	// Method to close the database connection, statement, and result set
-	public static void close(Connection conn, Statement stmt, ResultSet rs) {
-
-		try {
-			if (rs != null) {
-				rs.close(); // Close the result set if not null
-			}
-			if (stmt != null) {
-				stmt.close(); // Close the statement if not null
-			}
-			if (conn != null) {
-				conn.close(); // Close the connection if not null
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			// No need to rethrow the exception since it's just closing the resources.
-		}
-	}
 }
